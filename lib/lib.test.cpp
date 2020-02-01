2e4a74294e1b45c0238e0b37d1b9faae055fd28e
XzoRit <xzorit@hotmail.com>
2020-02-01 17:23:14 +0100

    change code format

--- lib/lib.test.cpp
+++ lib/lib.test.cpp
@@ -1,12 +1,13 @@
 #include <lib/lib.hpp>
 
 #include <cassert>
+#include <tuple>
+#include <type_traits>
 #include <vector>
 
 #include <boost/ut.hpp>
 
-namespace
-{
+namespace {
 using namespace boost::ut;
 
 using xzr::lib::add;
@@ -50,7 +51,7 @@ suite ut_assertions = [] {
   };
 
   "fatal"_test = [] {
-    std::vector v = { 1, 2, 3 };
+    std::vector v = {1, 2, 3};
 
     !expect(v.size() == 3_u);
     expect(v[0] == 1_i);
@@ -84,7 +85,8 @@ suite ut_sections = [] {
 
 suite ut_exceptions_abort = [] {
   "exceptions"_test = [] {
-    expect(throws<std::runtime_error>([] { throw std::runtime_error(""); })) << "throws runtime_error";
+    expect(throws<std::runtime_error>([] { throw std::runtime_error(""); }))
+        << "throws runtime_error";
     expect(throws([] { throw 0; })) << "throws any exception";
     expect(nothrow([] {})) << "does not throw";
   };
@@ -92,14 +94,48 @@ suite ut_exceptions_abort = [] {
   "aborts"_test = [] { expect(aborts([] { assert(false); })); };
 };
 
-suite ut_matchers = [] {};
-
 suite ut_param_tests = [] {
-  "param test"_test =
-    [](const auto& a) {
-      expect(that % a > 0) << "all values are greater than 0";
-    } |
-    std::vector{ 1, 2, 3, -1 };
+  "args"_test = [](const auto& a) {
+    expect(that % a > 0) << "all values are greater than 0";
+  } | std::vector{1, 2, 3, -1};
+
+  // only with c++20
+  // "types"_test = []<class T> {
+  //   expect(std::is_integral_v<T>) << "type shall be integral type";
+  // }
+  // | std::tuple<bool, int>{};
+
+  // "args and types"_test = []<class TArg>(TArg arg) {
+  //   !expect(std::is_integral_v<TArg>);
+  //   expect(42_i == arg or true_b == arg);
+  //   expect(type<TArg> == type<int> or type<TArg> == type<bool>);
+  // }
+  // | std::tuple{ true, 42 };
+};
+
+suite ut_log = [] {
+  log << "pre";
+  expect(1_i == 2) << "check log messages";
+  log << "post";
+};
+
+suite ut_skip_tests = [] {
+  skip |
+      "skip this test"_test = [] { expect(42_i == 43) << "should not fire"; };
+};
+
+suite ut_matcher = [] {
+  constexpr auto is_between = [](auto a, auto b) {
+    return [=](auto val) { return that % val >= a and that % val <= b; };
+  };
+
+  "matcher_42_is_between_1_100"_test = [is_between] {
+    expect(is_between(1, 100)(42));
+  };
+
+  "matcher_0_is_not_between_1_100"_test = [is_between] {
+    expect(not is_between(1, 100)(0));
+  };
 };
 
 suite lib_tests = [] {
@@ -109,6 +145,4 @@ suite lib_tests = [] {
     expect(add(3, 3) == 6_i);
   };
 };
-}
-
-int main() {}
+}  // namespace
