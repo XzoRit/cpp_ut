83a60938c0c8962940e16cf6af7a755cd5897561
XzoRit <xzorit@hotmail.com>
2021-08-29 18:51:17 +0200

    add filter, tags, color und dry run comand line options

--- lib/main.test.cpp
+++ lib/main.test.cpp
@@ -3,16 +3,8 @@
 #include <string_view>
 #include <vector>
 
-int main(int argc, const char **argv)
-{
-    namespace ut = boost::ut;
-    using ut::utility::split;
-
-    if (argc > 1)
-    {
-        if (std::string_view{argv[1]} == "--help")
-        {
-            std::cout << R"(
+const char *help_msg =
+    R"(
 1. name filter pattern
 2. comma separated list of tags
 3. if "1" colors terminal output
@@ -26,11 +18,21 @@ test_exe "test*" "slow,nightly" "1" "1"
 
 colored list of all tests starting with "test" and are either tagged slow or nightly
 )";
+
+int main(int argc, const char **argv)
+{
+    namespace ut = boost::ut;
+
+    if (argc > 1)
+    {
+        if (std::string_view{argv[1]} == "--help")
+        {
+            std::cout << help_msg;
             return 0;
         }
     }
     ut::cfg<ut::override> = {.filter = argc > 1 ? argv[1] : "",
-                             .tag = split(std::string_view{argv[2]}, ','),
+                             .tag = ut::utility::split(std::string_view{argv[2]}, ','),
                              .colors = argc > 3 and argv[3][0] == '1' ? ut::colors{}
                                                                       : ut::colors{.none = "", .pass = "", .fail = ""},
                              .dry_run = argc > 4 ? argv[4][0] == '1' : false};
